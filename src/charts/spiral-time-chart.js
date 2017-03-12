1ec347ca5473b9021203dc30882c7685fc72f626
ginpei <ginpei@gmail.com>
2017-03-12 15:02:26 -0700

    spiral time chart (mock)

---
+++ src/charts/spiral-time-chart.js
@@ -0,0 +1,50 @@
+import ChartBase from './chart-base.js';
+
+const d3 = require('d3');
+
+export default class SpiralTimeChart extends ChartBase {
+	constructor(options) {
+		super(options);
+	}
+
+	start() {
+		const line = this.generateLine(this.data, this.width, this.height);
+		const svg = this.generateSvg(this.element, this.width, this.height);
+
+		svg.append('path')
+			.datum(this.data)
+			.attr('d', line);
+	}
+
+	/**
+	 * @param {Array} data
+	 * @param {number} width
+	 * @param {number} height
+	 */
+	generateLine(data, width, height) {
+		var fx = d3.scaleTime()
+			.range([0, width])
+			.domain(d3.extent(data, d=>d.date));
+
+		var fy = d3.scaleLinear()
+			.range([height, 0])
+			.domain(d3.extent(data, d=>d.value));
+
+		return d3.line()
+			.x(d=>fx(d.date))
+			.y(d=>fy(d.value));
+	}
+
+	/**
+	 * @param {d3 element} element
+	 * @param {number} width
+	 * @param {number} height
+	 */
+	generateSvg(element, width, height) {
+		return element.append('svg')
+			.attr('width', width)
+			.attr('height', height)
+			.style('fill', 'none')
+			.style('stroke', '#000');
+	}
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import SimpleLineChart from './charts/simple-line-chart.js';
+import SpiralTimeChart from './charts/spiral-time-chart.js';
 
 const d3 = require('d3');
 document.querySelector('#js-version').textContent = `v${d3.version}`;
@@ -6,4 +7,9 @@ document.querySelector('#js-version').textContent = `v${d3.version}`;
 new SimpleLineChart({
 	data: require('./data/monthly-values.js').default,
 	selector: '#simple-line-chart',
+}).start();
+
+new SpiralTimeChart({
+	data: require('./data/monthly-values.js').default,
+	selector: '#spiral-time-chart',
 }).start();
