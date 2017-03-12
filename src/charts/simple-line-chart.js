61da5e05316ec5cf9d77f60e3c9ed7a9d4d5ccaa
ginpei <ginpei@gmail.com>
2017-03-12 13:26:57 -0700

    extract to methods

--- src/charts/simple-line-chart.js
+++ src/charts/simple-line-chart.js
@@ -8,39 +8,33 @@ export default class SimpleLineChart extends ChartBase {
 	}
 
 	start() {
-		var width = this.width;
-		var height = this.height;
+		const line = this.generateLine(this.data, this.width, this.height);
+		const svg = this.generateSvg(this.element, this.width, this.height);
 
-		// something
-		// to convert values to coordinates
-		var x = d3.scaleTime().range([0,width]);
-		var y = d3.scaleLinear().range([height,0]);
-
-		// line generator
-		// to write a line (path)
-		var line = d3.line();
-		line.x(d=>x(d.date));
-		line.y(d=>y(d.value));
-
-		// --------------------------------
-		// render a chart according to the data
-
-		var data = this.data;
-
-		// desice extents of both axises
-		x.domain(d3.extent(data, d=>d.date));
-		y.domain(d3.extent(data, d=>d.value));
-
-		// create a root SVG element
-		var svg = this.element.append('svg');
-		svg.attr('width', this.width);
-		svg.attr('height', this.height);
-
-		// write a line
 		svg.append('path')
-			.datum(data)
-			.style('fill', 'none')
-			.style('stroke', '#000')
+			.datum(this.data)
 			.attr('d', line);
+	}
+
+	generateLine(data, width, height) {
+		var fx = d3.scaleTime()
+			.range([0, width])
+			.domain(d3.extent(data, d=>d.date));
+
+		var fy = d3.scaleLinear()
+			.range([height, 0])
+			.domain(d3.extent(data, d=>d.value));
+
+		return d3.line()
+			.x(d=>fx(d.date))
+			.y(d=>fy(d.value));
+	}
+
+	generateSvg(element, width, height) {
+		return element.append('svg')
+			.attr('width', width)
+			.attr('height', height)
+			.style('fill', 'none')
+			.style('stroke', '#000');
 	}
 }
