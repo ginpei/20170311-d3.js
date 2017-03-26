c2953ad56fbf962ff103ef1d02e2a4ae081d87e4
ginpei <ginpei@gmail.com>
2017-03-26 00:30:35 -0700

    test with babel

--- src/charts/chart-base.js
+++ src/charts/chart-base.js
@@ -4,14 +4,25 @@ export default class ChartBase {
 	constructor(options) {
 		this.data = options.data;
 
-		const domNode = this.domNode = document.querySelector(options.selector);
-		if (!this.domNode) {
-			throw new Error('Element not found by the selector `' + options.selector + '`.');
-		}
+		const domNode = this.constructor_initDomNode(options);
 		this.height = domNode.clientHeight;
 		this.width = domNode.clientWidth;
-
 		this.element = d3.select(domNode);
 	}
 
+	constructor_initDomNode(options) {
+		let domNode;
+		if (options.el) {
+			domNode = options.el;
+		}
+		else {
+			domNode = document.querySelector(options.selector);
+		}
+
+		if (!domNode) {
+			throw new Error('DOM node needs to be specified by `options.el` or `options.selector`.');
+		}
+
+		return this.domNode = domNode;
+	}
 }
--- test/foo.test.js
+++ test/foo.test.js
@@ -1,7 +1,24 @@
+import SpiralTimeChart from '../src/charts/spiral-time-chart.js';
+
 const expect = require('chai').expect;
+const moment = require('moment');
 
 describe('Foo Text', ()=>{
+	const originalData = [
+	];
+
+	let chart;
+
+	beforeEach(()=>{
+		const elCanvas = document.createElement('canvas');
+
+		chart = new SpiralTimeChart({
+			data: originalData,
+			el: elCanvas,
+		});
+	});
+
 	it('is OK', ()=>{
-		expect(true).to.equal(true);
+		expect(chart).not.to.equal(null);
 	});
 });
